d1a57d470ded0c408d4eb97762fa5241c17461f2
Albert Hakobyan <abhakobyan24@gmail.com>
2021-09-12 01:19:54 +0400

    Добавьте файлы проекта.

---
+++ MakeArrayConsecutive2_CodeSignal_Core_Test42/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeArrayConsecutive2_CodeSignal_Core_Test42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 5, 4, 6};
+            int res = makeArrayConsecutive2(array);
+            Console.WriteLine(res);
+        }
+
+        static int makeArrayConsecutive2(int[] statues)
+        {
+            Array.Sort(statues);
+            HashSet<int> result = new HashSet<int>();
+            result = statues.ToHashSet();
+            for (int i = statues[0]; i < statues[statues.Length - 1]; i++)
+            {
+                result.Add(i);
+            }
+            int x = result.Count;
+            int y = statues.Length;
+            int res = x - y;
+            return res;
+        }
+    }
+}
